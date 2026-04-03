4f37b58cbf504a01f2761d2449b55e4c97602ccf
BarrelPanda <samuel.leong.2006@gmail.com>
2026-04-03 16:26:28 +0800

    Reorganized UI and file items

--- Assets/Script/Scene_Change_Script.cs
+++ Assets/Script/Scene_Change_Script.cs
@@ -3,8 +3,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 
-//[CreateAssetMenu(fileName ="Scene_Change",menuName ="Scriptable Object/Scene",order =1)]
-public class Scene_Change_Script : MonoBehaviour//ScriptableObject
+public class Scene_Change_Script : MonoBehaviour
 {
     public int WhichLevelAreYou;
     public int NumberOfSceneInSetting;
@@ -15,46 +14,29 @@ public class Scene_Change_Script : MonoBehaviour//ScriptableObject
     {
         switch (nextSceneName)
         {
+            case "MainMenu":
+                SceneManager.LoadScene("LevelSelect");
+                break;
+            case "Quit":
+                Application.Quit();
+                break;
+            case "Level_1":
+                SceneManager.LoadScene(nextSceneName);
+                break;
             case "Level_2":
                 if (Score_In_Level.Instance.PointsNeededLevel1 >= 100)
                     SceneManager.LoadScene(nextSceneName);
                 else
                     ShowTextOnClick();
                     break;
-            case "Level_1":
-                SceneManager.LoadScene(nextSceneName);
-                break;
-            case "MainMenu":
-                SceneManager.LoadScene("LevelSelect");
-                break;
             default:
                 break;
         }
-        //if (WhichLevelAreYou == 1)
-        //{
-        //    SceneManager.LoadScene(NumberOfSceneInSetting);
-        //}
-        //else if (WhichLevelAreYou == 2)
-        //{
-        //    if (Score_In_Level.Instance.Score_Level_1 == 100)
-        //    {
-        //        SceneManager.LoadScene(NumberOfSceneInSetting);
-        //    }
-        //    else
-        //    {
-        //        ShowTextOnClick();
-        //    }
-        //}
-
-
     }
 
     public void ShowTextOnClick()
     {
-        //coroutine start
         MyTextObject.gameObject.SetActive(true);
         MyTextObject.text = "Not enough point";
-        //coroutine check and wait 5 second then continue
-        //MyTextObject.gameObject.SetActive(false);
     }
 }
--- Assets/Script/Score_In_Level.cs
+++ Assets/Script/Score_In_Level.cs
@@ -1,9 +1,11 @@
+using TMPro;
 using UnityEngine;
 
 public class Score_In_Level : MonoBehaviour
 {
     public static Score_In_Level Instance;
     public int PointsNeededLevel1 = 0;
+    public TMP_Text ScoreUI1;
 
     private void Awake()
     {
