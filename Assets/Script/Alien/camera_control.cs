627fa22e893e9b627851a93ee3eff9345c3bc0e5
BarrelPanda <samuel.leong.2006@gmail.com>
2026-04-10 15:59:53 +0800

    Added UI

--- Assets/Script/Alien/camera_control.cs
+++ Assets/Script/Alien/camera_control.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class camera_control : MonoBehaviour
 {
-    [SerializeField] private Vector3 alienScreenPosition;
-    [SerializeField] private Vector3 bakeryScreenPosition;
     [SerializeField] private float camTravelSpeed = 5;
 
+    [Header("Alien")]
+    [SerializeField] private RectTransform alienButton;
+    [SerializeField] private Vector3 alienScreenPosition;
+
+    [Header("Bakery")]
+    [SerializeField] private RectTransform bakeryButton;
+    [SerializeField] private Vector3 bakeryScreenPosition;
+
     private bool alienScreen;
+    private Vector3 showButton = new Vector3(10, 0, 0);
+    private Vector3 hideButton = new Vector3(50, 0, 0);
 
     void Start()
     {
@@ -17,13 +25,18 @@ public class camera_control : MonoBehaviour
     void Update()
     {
         if (alienScreen)
+        {
             transform.position = Vector3.Lerp(transform.position, alienScreenPosition, camTravelSpeed * Time.deltaTime);
+            alienButton.anchoredPosition = Vector3.Lerp(alienButton.anchoredPosition, -hideButton, camTravelSpeed * Time.deltaTime);
+            bakeryButton.anchoredPosition = Vector3.Lerp(bakeryButton.anchoredPosition, -showButton, camTravelSpeed * Time.deltaTime);
+        }
         else
+        {
             transform.position = Vector3.Lerp(transform.position, bakeryScreenPosition, camTravelSpeed * Time.deltaTime);
+            alienButton.anchoredPosition = Vector3.Lerp(alienButton.anchoredPosition, showButton, camTravelSpeed * Time.deltaTime);
+            bakeryButton.anchoredPosition = Vector3.Lerp(bakeryButton.anchoredPosition, hideButton, camTravelSpeed * Time.deltaTime);
+        }
     }
 
-    public void SwitchScreen(bool toAlien)
-    {
-        alienScreen = toAlien;
-    }
+    public void SwitchScreen(bool toAlien) { alienScreen = toAlien; }
 }
